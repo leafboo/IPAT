d8c919cf050ed632ad4a31a00ab889fe9805fd8d
Jan <110758056+leafboo@users.noreply.github.com>
2024-10-09 15:18:34 +0800

    Add files via upload

---
+++ DB_ACT1_046_Caranguian/frmMain.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace DB_ACT1_046_Caranguian
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtKeyword = new System.Windows.Forms.TextBox();
+            this.cboBrand = new System.Windows.Forms.ComboBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.grdResults = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grdResults)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtKeyword
+            // 
+            this.txtKeyword.Location = new System.Drawing.Point(44, 48);
+            this.txtKeyword.Name = "txtKeyword";
+            this.txtKeyword.Size = new System.Drawing.Size(160, 20);
+            this.txtKeyword.TabIndex = 0;
+            this.txtKeyword.TextChanged += new System.EventHandler(this.txtKeyword_TextChanged);
+            // 
+            // cboBrand
+            // 
+            this.cboBrand.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboBrand.FormattingEnabled = true;
+            this.cboBrand.Items.AddRange(new object[] {
+            "Acura",
+            "Aston Martin",
+            "Audi",
+            "Bentley",
+            "BMW",
+            "Buick",
+            "Cadillac",
+            "Chevrolet",
+            "Chrysler",
+            "Dodge",
+            "Eagle",
+            "Ford",
+            "Geo",
+            "GMC",
+            "Honda",
+            "Hummer",
+            "Hyundai",
+            "Infiniti",
+            "Isuzu",
+            "Jeep",
+            "Kia",
+            "Land Rover",
+            "Lexus",
+            "Lincoln",
+            "Lotus",
+            "Maserati",
+            "Mazda",
+            "Mercedes-Benz",
+            "Mercury",
+            "Mitsubishi",
+            "Nissan",
+            "Oldsmobile",
+            "Plymouth",
+            "Pontiac",
+            "Porsche",
+            "Ram",
+            "Saab",
+            "Saturn",
+            "Scion",
+            "Shelby",
+            "Spyker",
+            "Subaru",
+            "Suzuki",
+            "Toyota",
+            "Volkswagen",
+            "Volvo"});
+            this.cboBrand.Location = new System.Drawing.Point(227, 48);
+            this.cboBrand.Name = "cboBrand";
+            this.cboBrand.Size = new System.Drawing.Size(121, 21);
+            this.cboBrand.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(44, 88);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(140, 88);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // grdResults
+            // 
+            this.grdResults.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.grdResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdResults.Location = new System.Drawing.Point(44, 130);
+            this.grdResults.MultiSelect = false;
+            this.grdResults.Name = "grdResults";
+            this.grdResults.ReadOnly = true;
+            this.grdResults.RowHeadersVisible = false;
+            this.grdResults.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grdResults.Size = new System.Drawing.Size(601, 281);
+            this.grdResults.TabIndex = 4;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 450);
+            this.Controls.Add(this.grdResults);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.cboBrand);
+            this.Controls.Add(this.txtKeyword);
+            this.Name = "frmMain";
+            this.Text = "Main";
+            ((System.ComponentModel.ISupportInitialize)(this.grdResults)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKeyword;
+        private System.Windows.Forms.ComboBox cboBrand;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.DataGridView grdResults;
+    }
+}
+
---
+++ DB_ACT1_046_Caranguian/frmMain.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace DB_ACT1_046_Caranguian
+{
+    public partial class frmMain : Form
+    {
+        // does this run if the mysql workbench is turned off?
+        string connectionStr = "server=localhost; database=db_act1; pwd=uslt; uid=root; port=3306";
+        MySqlConnection conn;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            searchCar();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtKeyword.Clear();
+            grdResults.DataSource = null;
+            grdResults.Rows.Clear();
+            cboBrand.SelectedIndex = -1;
+        }
+
+        private void txtKeyword_TextChanged(object sender, EventArgs e)
+        {
+            searchCar();
+        }
+
+        void searchCar()
+        {
+            if (string.IsNullOrEmpty(cboBrand.Text)) {
+                txtKeyword.Clear();
+                MessageBox.Show("Select a car brand first");
+            } else{
+                DataTable dt = new DataTable();
+
+                dt.Columns.Add("MODEL");
+                dt.Columns.Add("BRAND");
+                dt.Columns.Add("YEAR");
+
+                conn = new MySqlConnection(connectionStr);
+                string query = ($"SELECT model, brand, year FROM car WHERE model LIKE '{txtKeyword.Text}%' AND brand='{cboBrand.Text}';");
+                conn.Open();
+                MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn);
+                adapter.Fill(dt);
+                conn.Close();
+                grdResults.DataSource = dt;
+            }
+           
+        }
+    }
+}
